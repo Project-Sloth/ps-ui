c27ff0be1c2f813785ea636cb9654b030002a588
Rian Lotter <rianlotter@icloud.com>
2024-10-16 17:35:47 +0200

    Fix: Prevent GameBase from re-rendering multiple times.

--- web/src/games/GameLauncher.svelte
+++ web/src/games/GameLauncher.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import Skull from '../assets/svgs/Skull.svelte';
 	('./../../utils/mockEvent');
 	import { connectionText, showLoading } from '../stores/GameLauncherStore';
@@ -9,6 +9,7 @@
 	const skullColor: string = '#02f1b5';
 
 	let loadingBar: HTMLDivElement;
+	let gameLoaded = false;
 
 	/** Asynchronously connects to a game and resolves the Promise when completed.
 	 * Uses a loading bar to show progress, incrementing by 1% every 30ms until it reaches 100%.
@@ -41,11 +42,15 @@
 
 		// Hide loading indicator when completed connecting to the game server
 		connectionText.set(ConnectingGameMessageEnum.Connected);
-
 		setTimeout(() => {
 			showLoading.set(false);
+			gameLoaded = true;
 		}, 2000);
 	});
+
+	onDestroy(async () => {
+		gameLoaded = false
+	})
 </script>
 
 {#if $showLoading}
@@ -63,13 +68,12 @@
 					bind:this={loadingBar}
 					class="ps-bg-green opacity-40 will-change-auto w-0"
 				>
-					l
 				</div>
 			</div>
 		</div>
 	</div>
 {/if}
 
-{#if !$showLoading}
+{#if !$showLoading && gameLoaded}
 	<GameBase />
 {/if}
